bf3d29fb4dd3f4df9cfcf9d7758a1ea3f090981e
Anar Bairam <anar.bayram@yahoo.com>
2023-02-13 11:15:24 +0400

    note schema and note list view created

--- app/note.py
+++ app/note.py
@@ -9,3 +9,12 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+class Note(BaseModel):
+    title: str
+    content: str
+    till_date: datetime
+
+@app.get('/')
+def note_list(db: Session = Depends(get_db)):
+    notes = db.query(models.Note).all()
+    return {'data': notes}
